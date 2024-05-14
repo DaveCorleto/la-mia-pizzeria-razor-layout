472aeb8b7da979a12ff0a6a968e7139eb3d53331
DaveCorleto <davide.corleto@fastwebnet.it>
2024-05-14 19:57:06 +0200

    prove varie

---
+++ Views/Shared/_footer.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Test_MVC_2.Views.Shared
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
